179b402ca4bf21107e559bdc7ac2ce6528422c9d
Sandeep Kumar <ki.sandeep11@gmail.com>
2020-11-30 16:48:40 +0530

    Code enhancement

--- app/auth.js
+++ app/auth.js
@@ -13,7 +13,6 @@ app.use(formData.array());
 router.post('/login', async (req, res) => {
 
     try {
-
         let rows = await conn.query("SELECT t1.token from users as t1 where t1.email='" + req.body.email + "' and t1.password='" + req.body.password + "' LIMIT 1")
         if (rows[0]) {
             res.status(200).json({ data: rows[0], message: 'Success' })
@@ -21,7 +20,6 @@ router.post('/login', async (req, res) => {
         else {
             res.status(404).json({ data: [], message: 'Not found' })
         }
-
     }
     catch (err) {
         res.status(403).json({ message: err });
